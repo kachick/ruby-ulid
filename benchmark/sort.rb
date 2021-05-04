adbeef6ce9e4d09a5db6e1f3ac7ec3093eb020b7
Kenichi Kamiya <kachick1@gmail.com>
2021-05-05 02:47:35 +0900

    Improve benchmarks for sorting

--- benchmark/sort.rb
+++ benchmark/sort.rb
@@ -1,31 +1,34 @@
 require 'benchmark/ips'
 require_relative '../lib/ulid'
 
-monotonic_generator = ULID::MonotonicGenerator.new
-ulid_objects = 10000.times.map do
-  monotonic_generator.generate
+frozen_ulid_objects = 10000.times.map do
+  ULID.generate.freeze
 end
-ulid_strings = ulid_objects.map(&:to_s)
-unless ulid_objects == ulid_objects.sort
-  raise 'Setup error'
-end
-
-unless ulid_strings == ulid_strings.sort
-  raise 'Setup error'
-end
-
-unless ulid_strings.sort == ulid_objects.sort.map(&:to_s)
-  raise 'Setup error'
-end
+ulid_strings = frozen_ulid_objects.map(&:to_s)
+non_cached_ulid_objects = frozen_ulid_objects.map(&:dup)
 
 Benchmark.ips do |x|
-  x.report('Sort in ULID object') do
-    ulid_objects.shuffle.sort
+  x.report('Sort ULID instance') do
+    non_cached_ulid_objects.shuffle.sort # After second sorting, cache will be work. So this benchmark is not accurate.
   end
 
-  x.report('Sort in String') do
+  x.report('Sort frozen ULID instance(some values cached)') do
+    frozen_ulid_objects.shuffle.sort
+  end
+
+  x.report('Sort by String encoded ULIDs') do
     ulid_strings.shuffle.sort
   end
 
   x.compare!
+end
+
+# I'll check to be sure sorting result is correct.
+
+unless ulid_strings == non_cached_ulid_objects.map(&:to_s)
+  raise 'Crucial Bug exists!'
+end
+
+unless ulid_strings == frozen_ulid_objects.map(&:to_s)
+  raise 'Crucial Bug exists!'
 end
