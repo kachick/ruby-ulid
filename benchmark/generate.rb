bc7595314ae299eab65b02d47cd3949665b86df2
Kenichi Kamiya <kachick1@gmail.com>
2021-05-05 02:19:39 +0900

    Add benchmarks to ensure `ULID.min` and `ULID.max` is optimized
    Since d9d5b3914f62edb8fef82bcfefa9aa2ee1538b20, they returns same object.
    
    ```console
    $ ruby -v
    ruby 3.0.1p64 (2021-04-05 revision 0fb782ee38) [x86_64-darwin20]
    
    $ bundle exec ruby benchmark/generate.rb
    Warming up --------------------------------------
           ULID.generate    38.978k i/100ms
    Monotonic generating    49.007k i/100ms
    ULID.min with no arguments is optimized
                             1.060M i/100ms
    ULID.max with no arguments is optimized
                           848.592k i/100ms
    Calculating -------------------------------------
           ULID.generate    390.567k (± 3.7%) i/s -      1.988M in   5.096732s
    Monotonic generating    466.934k (± 7.5%) i/s -      2.352M in   5.069575s
    ULID.min with no arguments is optimized
                             10.083M (± 3.9%) i/s -     50.896M in   5.055541s
    ULID.max with no arguments is optimized
                              7.850M (± 1.8%) i/s -     39.884M in   5.082575s
    
    Comparison:
    ULID.min with no arguments is optimized: 10083485.4 i/s
    ULID.max with no arguments is optimized:  7849596.2 i/s - 1.28x  (± 0.00) slower
    Monotonic generating:   466934.2 i/s - 21.60x  (± 0.00) slower
           ULID.generate:   390567.2 i/s - 25.82x  (± 0.00) slower
    ```
    
    Looks `better`, Not as good as I thought.
    But the min/max cached results as `to_i`, `to_s` and more.
    So should be better in actual use-case, I hope...

--- benchmark/generate.rb
+++ benchmark/generate.rb
@@ -6,5 +6,7 @@ monotonic_generator = ULID::MonotonicGenerator.new
 Benchmark.ips do |x|
   x.report('ULID.generate') { ULID.generate }
   x.report('Monotonic generating') { monotonic_generator.generate }
+  x.report('ULID.min with no arguments is optimized') { ULID.min }
+  x.report('ULID.max with no arguments is optimized') { ULID.max }
   x.compare!
 end
