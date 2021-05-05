c7876bb1c28fbc53462906f7a297872853c60d00
Kenichi Kamiya <kachick1@gmail.com>
2021-05-06 04:31:29 +0900

    Add fixed values tests to prevent breaking changes

---
+++ test/many_data/fixtures/example.rb
@@ -0,0 +1,4 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+Example = Struct.new(:string, :integer, :timestamp, :randomness, :time, :to_time, :inspect, :uuidv4, keyword_init: true)
---
+++ test/many_data/test_fixed_many_data.rb
@@ -0,0 +1,58 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+require_relative '../helper'
+require_relative 'fixtures/example'
+
+dump_data = File.binread("#{__dir__}/fixtures/dumped_fixed_examples.dat")
+EXAMPLES = Marshal.load(dump_data)
+
+# https://github.com/kachick/ruby-ulid/issues/89
+class TestFixedManyData < Test::Unit::TestCase
+  def assert_example(ulid, example)
+    assert_equal(example.string, ulid.to_s)
+    assert_equal(example.integer, ulid.to_i)
+    assert_equal(example.inspect, ulid.inspect)
+    assert_equal(example.timestamp, ulid.timestamp)
+    assert_equal(example.randomness, ulid.randomness)
+    assert_equal(example.uuidv4, ulid.to_uuidv4)
+    assert_equal(example.to_time, ulid.to_time)
+    assert_equal(ULID.floor(example.time), ulid.to_time)
+  end
+
+  def test_many_fixed_examples_for_from_integer
+    EXAMPLES.each do |example|
+      ulid = ULID.from_integer(example.integer)
+      assert_example(ulid, example)
+    end
+  end
+
+  def test_many_fixed_examples_for_parse
+    EXAMPLES.each do |example|
+      ulid = ULID.parse(example.string)
+      assert_example(ulid, example)
+    end
+  end
+
+  def test_many_fixed_examples_for_parse_with_integer_base
+    EXAMPLES.each do |example|
+      ulid = ULID.parse_with_integer_base(example.string)
+      assert_example(ulid, example)
+    end
+  end
+
+  # Fix ME!
+  def test_many_fixed_examples_from_uuidv4
+    non_reversible_ulid_strings = []
+    EXAMPLES.each do |example|
+      ulid = ULID.from_uuidv4(example.uuidv4)
+      unless example.string == ulid.to_s
+        non_reversible_ulid_strings << ulid.to_s
+        next # found many unexpected cases... :< ref: https://github.com/kachick/ruby-ulid/issues/76
+      end
+      assert_example(ulid, example)
+    end
+    puts non_reversible_ulid_strings.size
+    puts non_reversible_ulid_strings.take(10)
+  end
+end
