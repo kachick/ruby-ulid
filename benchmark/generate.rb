3240750dcdda6bb54fb2d9da4e3c24012077a62f
Kenichi Kamiya <kachick1@gmail.com>
2021-05-06 23:43:04 +0900

    Add benchmark for ULID.from_integer
    [ci skip]
    
    ```console
    $ ruby -v
    ruby 3.0.1p64 (2021-04-05 revision 0fb782ee38) [x86_64-darwin20]
    
    $ bundle exec ruby benchmark/generate.rb
    Warming up --------------------------------------
           ULID.generate    42.171k i/100ms
    ULID::MonotonicGenerator#generate
                            53.731k i/100ms
       ULID.from_integer    18.922k i/100ms
    ULID.min with no arguments is optimized
                             1.163M i/100ms
    ULID.max with no arguments is optimized
                           922.477k i/100ms
    Calculating -------------------------------------
           ULID.generate    429.594k (± 2.8%) i/s -      2.151M in   5.010395s
    ULID::MonotonicGenerator#generate
                            519.420k (± 4.6%) i/s -      2.633M in   5.080871s
       ULID.from_integer    179.136k (± 3.8%) i/s -    908.256k in   5.077560s
    ULID.min with no arguments is optimized
                             11.316M (± 3.7%) i/s -     57.006M in   5.044787s
    ULID.max with no arguments is optimized
                              8.765M (± 3.8%) i/s -     44.279M in   5.059453s
    
    Comparison:
    ULID.min with no arguments is optimized: 11315766.4 i/s
    ULID.max with no arguments is optimized:  8764667.6 i/s - 1.29x  (± 0.00) slower
    ULID::MonotonicGenerator#generate:   519419.8 i/s - 21.79x  (± 0.00) slower
           ULID.generate:   429594.4 i/s - 26.34x  (± 0.00) slower
       ULID.from_integer:   179136.3 i/s - 63.17x  (± 0.00) slower
    ```

--- benchmark/generate.rb
+++ benchmark/generate.rb
@@ -5,7 +5,8 @@ monotonic_generator = ULID::MonotonicGenerator.new
 
 Benchmark.ips do |x|
   x.report('ULID.generate') { ULID.generate }
-  x.report('Monotonic generating') { monotonic_generator.generate }
+  x.report('ULID::MonotonicGenerator#generate') { monotonic_generator.generate }
+  x.report('ULID.from_integer') { ULID.from_integer(SecureRandom.random_number(ULID::MAX_INTEGER)) }
   x.report('ULID.min with no arguments is optimized') { ULID.min }
   x.report('ULID.max with no arguments is optimized') { ULID.max }
   x.compare!
