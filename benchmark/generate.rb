248d15870b1f1f7f63778b8a1615b9439eb2fc95
Kenichi Kamiya <kachick1@gmail.com>
2021-05-07 11:32:08 +0900

    Add fixed value benchmark for a preparation part of #100
    ```console
    $ ruby -v
    ruby 3.0.1p64 (2021-04-05 revision 0fb782ee38) [x86_64-darwin20]
    
    $ bundle exec ruby benchmark/generate.rb
    Warming up --------------------------------------
           ULID.generate    44.536k i/100ms
    ULID::MonotonicGenerator#generate
                            54.536k i/100ms
    ULID.from_integer with random value
                            18.970k i/100ms                                                                    ULID.from_integer with fixed integer `311111255230011988062198468225631077989` (To compare randomizing over
    head)
                            18.805k i/100ms
    ULID.min with no arguments is optimized
                             1.151M i/100ms
    ULID.max with no arguments is optimized
                           944.548k i/100ms
             ULID.sample    18.866k i/100ms
    Calculating -------------------------------------
           ULID.generate    429.889k (± 3.3%) i/s -      2.182M in   5.081990s
    ULID::MonotonicGenerator#generate
                            523.125k (± 3.4%) i/s -      2.618M in   5.009806s
    ULID.from_integer with random value
                            173.448k (± 7.7%) i/s -    872.620k in   5.063104s
    ULID.from_integer with fixed integer `311111255230011988062198468225631077989` (To compare randomizing over
    head)
                            177.064k (± 5.1%) i/s -    883.835k in   5.004816s
    ULID.min with no arguments is optimized
                             11.036M (± 4.9%) i/s -     55.226M in   5.016989s
    ULID.max with no arguments is optimized
                              8.911M (± 4.3%) i/s -     45.338M in   5.097789s
             ULID.sample    178.248k (± 3.5%) i/s -    905.568k in   5.086830s
    
    Comparison:
    ULID.min with no arguments is optimized: 11035971.6 i/s
    ULID.max with no arguments is optimized:  8910507.4 i/s - 1.24x  (± 0.00) slower
    ULID::MonotonicGenerator#generate:   523124.5 i/s - 21.10x  (± 0.00) slower
           ULID.generate:   429889.4 i/s - 25.67x  (± 0.00) slower
             ULID.sample:   178247.7 i/s - 61.91x  (± 0.00) slower
    ULID.from_integer with fixed integer `311111255230011988062198468225631077989` (To compare randomizing over
    head):   177064.1 i/s - 62.33x  (± 0.00) slower
    ULID.from_integer with random value:   173448.3 i/s - 63.63x  (± 0.00) slower
    ```
    
    [ci skip]

--- benchmark/generate.rb
+++ benchmark/generate.rb
@@ -5,11 +5,13 @@ require 'benchmark/ips'
 require_relative '../lib/ulid'
 
 monotonic_generator = ULID::MonotonicGenerator.new
+fixed_integer = SecureRandom.random_number(ULID::MAX_INTEGER)
 
 Benchmark.ips do |x|
   x.report('ULID.generate') { ULID.generate }
   x.report('ULID::MonotonicGenerator#generate') { monotonic_generator.generate }
-  x.report('ULID.from_integer') { ULID.from_integer(SecureRandom.random_number(ULID::MAX_INTEGER)) }
+  x.report('ULID.from_integer with random value') { ULID.from_integer(SecureRandom.random_number(ULID::MAX_INTEGER)) }
+  x.report("ULID.from_integer with fixed integer `#{fixed_integer}` (To compare randomizing overhead)") { ULID.from_integer(SecureRandom.random_number(ULID::MAX_INTEGER)) }
   x.report('ULID.min with no arguments is optimized') { ULID.min }
   x.report('ULID.max with no arguments is optimized') { ULID.max }
   x.report('ULID.sample') { ULID.sample }
