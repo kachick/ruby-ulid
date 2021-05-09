81b81ebfb23fe6d0c03372f92aec80b169e6e027
Kenichi Kamiya <kachick1@gmail.com>
2021-05-09 12:09:55 +0900

    Improve test strategy
    * Simplify file glob with the directory naming convention
    * Split light and core features tests and heavy tests
    * Special assertions into TestUnit namespace, not global
    
    This commit improves #87, #60, #89

--- test/core/test_ulid_example_values.rb
+++ test/core/test_ulid_example_values.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-require_relative 'helper'
+require_relative '../helper'
 
 class TestULIDWithExampleValues < Test::Unit::TestCase
   def test_oklog_examples
--- test/core/test_ulid_usecase.rb
+++ test/core/test_ulid_usecase.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-require_relative 'helper'
+require_relative '../helper'
 
 class TestULIDUseCase < Test::Unit::TestCase
   def test_range_elements
