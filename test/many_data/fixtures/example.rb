7edb43150861b334ed1c6edc280a77724f137f70
Kenichi Kamiya <kachick1@gmail.com>
2021-05-09 14:10:31 +0900

    Refine fixed test fixtures

--- test/many_data/fixtures/example.rb
+++ test/many_data/fixtures/example.rb
@@ -1,4 +1,4 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-Example = Struct.new(:string, :integer, :timestamp, :randomness, :time, :to_time, :inspect, :uuidv4, keyword_init: true)
+Example = Struct.new(:string, :integer, :timestamp, :randomness, :period, :to_time, :inspect, :uuidv4, :octets, keyword_init: true)
--- test/many_data/test_fixed_many_data.rb
+++ test/many_data/test_fixed_many_data.rb
@@ -7,10 +7,14 @@ require_relative 'fixtures/example'
 
 # https://github.com/kachick/ruby-ulid/issues/89
 class TestFixedManyData < Test::Unit::TestCase
-  dump_data = File.binread("#{__dir__}/fixtures/dumped_fixed_examples.dat")
+  dump_data = File.binread("#{__dir__}/fixtures/dumped_fixed_examples_2021-05-09_06-22.bin")
   EXAMPLES = Marshal.load(dump_data)
 
   def assert_example(ulid, example)
+    assert do
+      example.period.cover?(ulid.to_time)
+    end
+
     assert_equal(example.string, ulid.to_s)
     assert_equal(example.integer, ulid.to_i)
     assert_equal(example.inspect, ulid.inspect)
@@ -18,8 +22,11 @@ class TestFixedManyData < Test::Unit::TestCase
     assert_equal(example.randomness, ulid.randomness)
     assert_equal(example.uuidv4, ulid.to_uuidv4)
     assert_equal(example.to_time, ulid.to_time)
-    assert_equal(ULID.floor(example.time), ulid.to_time)
-    assert_equal(true, ULID.valid?(example.string))
+    assert_equal(example.octets, ulid.octets)
+
+    assert do
+      ULID.valid?(example.string)
+    end
   end
 
   def test_many_fixed_examples_for_from_integer
@@ -51,16 +58,17 @@ class TestFixedManyData < Test::Unit::TestCase
 
   # Fix ME!
   def test_many_fixed_examples_from_uuidv4
-    non_reversible_ulid_strings = []
+    weird_uuid_by_ulid = {}
     EXAMPLES.each do |example|
       ulid = ULID.from_uuidv4(example.uuidv4)
-      unless example.string == ulid.to_s
-        non_reversible_ulid_strings << ulid.to_s
-        next # found many unexpected cases... :< ref: https://github.com/kachick/ruby-ulid/issues/76
+      assert_equal(example.uuidv4, ulid.to_uuidv4, 'Basically reversible...')
+      unless example.string == ulid.to_s # Why this happened...
+        weird_uuid_by_ulid[ulid.to_s] = [example.string, example.uuidv4]
+        next
       end
       assert_example(ulid, example)
     end
-    puts non_reversible_ulid_strings.size
-    puts non_reversible_ulid_strings.take(10)
+    puts "All #{EXAMPLES.size} patterns `reversible`, but #{weird_uuid_by_ulid.size}/#{EXAMPLES.size} patterns are not mapping to same ULID... might relate to https://github.com/kachick/ruby-ulid/issues/76 ?"
+    puts 'Samples are below', weird_uuid_by_ulid.to_a.sample(5).to_h
   end
 end
