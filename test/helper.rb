b524beabdf6df3d17b4a24640edf5fad63828830
Kenichi Kamiya <kachick1@gmail.com>
2021-05-17 01:35:59 +0900

    Introduce irb-power_assert gem for my own

--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,7 @@ require 'test/unit'
 
 require 'irb'
 require 'power_assert/colorize'
+require 'irb/power_assert'
 
 if Warning.respond_to?(:[]=) # @TODO Removable this guard after dropped ruby 2.6
   Warning[:deprecated] = true
