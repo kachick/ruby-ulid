76aa4ffcddd18f4e751e6b9bb206dbc08b4b9fa0
Kenichi Kamiya <kachick1@gmail.com>
2022-07-18 14:48:04 +0900

    Extract parser bench from generator bench

--- benchmark/generators.rb
+++ benchmark/generators.rb
@@ -5,18 +5,14 @@ require('benchmark/ips')
 require_relative('../lib/ulid')
 
 monotonic_generator = ULID::MonotonicGenerator.new
-fixed_integer = SecureRandom.random_number(ULID::MAX_INTEGER)
 sample = ULID.sample.freeze
 moment = sample.to_time
 period = moment..(moment + 10000000)
-encoded = sample.to_s
 
 Benchmark.ips do |x|
   x.report('ULID.generate') { ULID.generate }
   x.report('ULID.encode') { ULID.encode }
   x.report('ULID::MonotonicGenerator#generate') { monotonic_generator.generate }
-  x.report('ULID.parse') { ULID.parse(encoded) }
-  x.report('ULID.from_integer') { ULID.from_integer(fixed_integer) }
   x.report('ULID.min with no arguments is optimized') { ULID.min }
   x.report('ULID.min with moment(Time)') { ULID.min(moment) }
   x.report('ULID.max with no arguments is optimized') { ULID.max }
---
+++ benchmark/parsers.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+require('benchmark/ips')
+require_relative('../lib/ulid')
+
+monotonic_generator = ULID::MonotonicGenerator.new
+fixed_integer = SecureRandom.random_number(ULID::MAX_INTEGER)
+sample = ULID.sample.freeze
+encoded = sample.to_s
+
+Benchmark.ips do |x|
+  x.report('ULID.parse') { ULID.parse(encoded) }
+  x.report('ULID.from_integer') { ULID.from_integer(fixed_integer) }
+  x.compare!
+end
