93a5c63093b3d1b1aa5a54edf0d1a00ec74bd1ad
Kenichi Kamiya <kachick1@gmail.com>
2022-07-20 22:18:43 +0900

    Add more bench for MonotonicGenerator
    ```
    ruby benchmark/monotonic_generators_with_now.rb
    Warming up --------------------------------------
             ULID.encode    16.269k i/100ms
    ULID::MonotonicGenerator#encode
                            22.119k i/100ms
    ULID::MonotonicGenerator#generate.to_s
                            21.863k i/100ms
    Calculating -------------------------------------
             ULID.encode    162.219k (± 0.9%) i/s -    813.450k in   5.014973s
    ULID::MonotonicGenerator#encode
                            216.571k (± 1.8%) i/s -      1.084M in   5.006183s
    ULID::MonotonicGenerator#generate.to_s
                            216.064k (± 2.0%) i/s -      1.093M in   5.061338s
    
    Comparison:
    ULID::MonotonicGenerator#encode:   216570.7 i/s
    ULID::MonotonicGenerator#generate.to_s:   216064.2 i/s - same-ish: difference falls within error
             ULID.encode:   162218.5 i/s - 1.34x  (± 0.00) slower
    ```
    
    ```
    ruby benchmark/monotonic_generators_with_different_time.rb
    Warming up --------------------------------------
    ULID.encode with different time
                            20.688k i/100ms
    ULID::MonotonicGenerator#encode with different time
                            15.633k i/100ms
    ULID::MonotonicGenerator#generate.to_s with different time
                            15.757k i/100ms
    Calculating -------------------------------------
    ULID.encode with different time
                            206.459k (± 0.6%) i/s -      1.034M in   5.010356s
    ULID::MonotonicGenerator#encode with different time
                            155.246k (± 0.8%) i/s -    781.650k in   5.035258s
    ULID::MonotonicGenerator#generate.to_s with different time
                            156.486k (± 1.0%) i/s -    787.850k in   5.035116s
    
    Comparison:
    ULID.encode with different time:   206458.8 i/s
    ULID::MonotonicGenerator#generate.to_s with different time:   156486.0 i/s - 1.32x  (± 0.00) slower
    ULID::MonotonicGenerator#encode with different time:   155246.1 i/s - 1.33x  (± 0.00) slower
    ```

--- benchmark/tools.rb
+++ benchmark/tools.rb
@@ -4,21 +4,16 @@
 require('benchmark/ips')
 require_relative('../lib/ulid')
 
-monotonic_generator = ULID::MonotonicGenerator.new
 sample = ULID.sample.freeze
 moment = sample.to_time
 period = moment..(moment + 10000000)
 
 Benchmark.ips do |x|
-  x.report('ULID.generate') { ULID.generate }
-  x.report('ULID.encode') { ULID.encode }
-  x.report('ULID::MonotonicGenerator#generate') { monotonic_generator.generate }
   x.report('ULID.min with no arguments is optimized') { ULID.min }
   x.report('ULID.min with moment(Time)') { ULID.min(moment) }
   x.report('ULID.max with no arguments is optimized') { ULID.max }
   x.report('ULID.max with moment(Time)') { ULID.max(moment) }
   x.report('ULID.sample with no arguments') { ULID.sample }
   x.report('ULID.sample with period') { ULID.sample(period: period) }
-  x.report('ULID.at') { ULID.at(Time.now) }
   x.compare!
 end
