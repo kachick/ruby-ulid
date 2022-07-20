cc38fb6de462ca386df9ed999fcde48f4cee2c14
Kenichi Kamiya <kachick1@gmail.com>
2022-07-20 16:55:12 +0900

    Add bench to compare parse(encoded)
    ```
    /home/kachick/.rubies/ruby-3.1.2/bin/ruby benchmark/generate_vs_encode.rb
    Warming up --------------------------------------
             ULID.encode    15.911k i/100ms
      ULID.generate.to_s    14.175k i/100ms
           ULID.generate    14.284k i/100ms
    ULID.parse(ULID.encode)
                             9.858k i/100ms
    Calculating -------------------------------------
             ULID.encode    163.134k (± 2.7%) i/s -    827.372k in   5.076016s
      ULID.generate.to_s    142.584k (± 2.6%) i/s -    722.925k in   5.073685s
           ULID.generate    143.053k (± 2.2%) i/s -    728.484k in   5.094885s
    ULID.parse(ULID.encode)
                             96.058k (± 1.8%) i/s -    483.042k in   5.030270s
    
    Comparison:
             ULID.encode:   163134.4 i/s
           ULID.generate:   143053.1 i/s - 1.14x  (± 0.00) slower
      ULID.generate.to_s:   142584.2 i/s - 1.14x  (± 0.00) slower
    ULID.parse(ULID.encode):    96058.3 i/s - 1.70x  (± 0.00) slower
    ```

--- benchmark/generate_vs_encode.rb
+++ benchmark/generate_vs_encode.rb
@@ -5,7 +5,9 @@ require('benchmark/ips')
 require_relative('../lib/ulid')
 
 Benchmark.ips do |x|
-  x.report('ULID.generate.to_s') { ULID.generate.to_s }
   x.report('ULID.encode') { ULID.encode }
+  x.report('ULID.generate.to_s') { ULID.generate.to_s }
+  x.report('ULID.generate') { ULID.generate }
+  x.report('ULID.parse(ULID.encode)') { ULID.parse(ULID.encode) }
   x.compare!
 end
