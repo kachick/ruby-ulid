7d2cfae6595231b1f12925467f9589bffbd668df
Kenichi Kamiya <kachick1@gmail.com>
2023-03-07 01:05:56 +0900

    Revert ULID#octets deprecation

--- test/many_data/test_fixed_many_data.rb
+++ test/many_data/test_fixed_many_data.rb
@@ -21,7 +21,6 @@ class TestFixedManyData < Test::Unit::TestCase
     assert_equal(example.randomness, ulid.randomness)
     assert_equal(example.uuidv4, ulid.to_uuidv4(ignore_reversible: true))
     assert_equal(example.to_time, ulid.to_time)
-    # ULID#octets is a deprecated method. But keeping it in example class for dump data compatibility
     assert_equal(example.octets, ulid.bytes)
 
     assert do
