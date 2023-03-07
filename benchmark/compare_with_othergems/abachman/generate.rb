6b63d7caefcfe3b907e3d23916c291545496ef1b
Kenichi Kamiya <kachick1@gmail.com>
2023-03-08 03:18:11 +0900

    Focus measuring targets in benchmark script
    Since Ruby 3.2.1, current benchmark code shows wrong result
    
    Removing non targetting code as possible makes better result again
    
    So closes #342, that is not the issue for now
    
    ```
    ❯ rake benchmark_with_other_gems
    ------------------------------------------------------------------------
    cd ./benchmark/compare_with_othergems/kachick
    bundle install --quiet
    bundle exec ruby -v ./generate.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    Warming up --------------------------------------
             ULID.encode    24.073k i/100ms
    Calculating -------------------------------------
             ULID.encode    237.226k (± 6.2%) i/s -      1.204M in   5.094403s
    bundle exec ruby -v ./parser.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    Warming up --------------------------------------
        ULID.decode_time    24.661k i/100ms
    Calculating -------------------------------------
        ULID.decode_time    253.593k (± 4.3%) i/s -      1.282M in   5.066831s
    cd -
    ------------------------------------------------------------------------
    cd ./benchmark/compare_with_othergems/rafaelsales
    bundle install --quiet
    bundle exec ruby -v ./generate.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    Warming up --------------------------------------
           ULID.generate    12.764k i/100ms
    Calculating -------------------------------------
           ULID.generate    125.398k (± 4.4%) i/s -    638.200k in   5.100201s
    bundle exec ruby -v ./parser.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    "`ulid gem - 1.4.0` does not have parsers yet"
    cd -
    ------------------------------------------------------------------------
    cd ./benchmark/compare_with_othergems/abachman
    bundle install --quiet
    bundle exec ruby -v ./generate.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    Warming up --------------------------------------
           ULID.generate    11.513k i/100ms
    Calculating -------------------------------------
           ULID.generate    114.931k (± 4.5%) i/s -    575.650k in   5.019902s
    bundle exec ruby -v ./parser.rb
    ruby 3.2.1 (2023-02-08 revision 31819e82c8) [x86_64-linux]
    Warming up --------------------------------------
               ULID.time     8.594k i/100ms
    Calculating -------------------------------------
               ULID.time     80.736k (± 5.4%) i/s -    403.918k in   5.018672s
    cd -
    ```

--- benchmark/compare_with_othergems/abachman/generate.rb
+++ benchmark/compare_with_othergems/abachman/generate.rb
@@ -6,21 +6,8 @@ require('ulid')
 
 raise("Bug to setup: #{ULID.methods(false)}") unless ULID.const_defined?(:Identifier)
 
-products = []
-
 Benchmark.ips do |x|
   x.report('ULID.generate') do
-    products << ULID.generate
+    ULID.generate
   end
 end
-
-# Below sections ensuring basic behaviors
-
-# This Regexp taken from my code. But case sensitive
-STRICT_PATTERN_WITH_CROCKFORD_BASE32_SUBSET = /\A(?<timestamp>[0-7][0123456789ABCDEFGHJKMNPQRSTVWXYZ]{9})(?<randomness>[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{16})\z/
-
-unless (products.size > 42) && (products.uniq === products) && products.all?(STRICT_PATTERN_WITH_CROCKFORD_BASE32_SUBSET) && products.all?(String)
-  raise('Some bugs in the gem or this benchmark exists!')
-end
-
-p("`ulid-ruby gem - #{ULID::VERSION}` generated products: #{products.size} - sample: #{products.sample(5)}")
--- benchmark/compare_with_othergems/kachick/parser.rb
+++ benchmark/compare_with_othergems/kachick/parser.rb
@@ -7,14 +7,9 @@ require('ulid')
 raise("Bug to setup: #{ULID.methods(false)}") unless ULID.const_defined?(:MonotonicGenerator)
 
 example_encoded_string = '01F4A5Y1YAQCYAYCTC7GRMJ9AA'
-products = []
 
 Benchmark.ips do |x|
   x.report('ULID.decode_time') do
-    products << ULID.decode_time(example_encoded_string)
+    ULID.decode_time(example_encoded_string)
   end
 end
-
-# Below sections ensuring basic behaviors
-
-p("`ruby-ulid gem (this one) - #{ULID::VERSION}` generated products: #{products.size} - sample: #{products.sample(5)}")
