e24a8ca669b2c3b3d494c654c9940fd20c78c043
Kenichi Kamiya <kachick1@gmail.com>
2024-03-10 13:48:07 +0900

    Make sure toml-rb raises similar error as taplo for distant future
    ```
    Failed to parse input on line 24007 at offset 24 (TomlRB::ParseError)
    to_time = 10889-07-21T15:54:33Z
    ```

--- scripts/convert_snapshot_dump_to_toml.rb
+++ scripts/convert_snapshot_dump_to_toml.rb
@@ -5,7 +5,7 @@ require('bundler/setup')
 require_relative('../lib/ulid')
 require_relative('../test/many_data/fixtures/example')
 
-require('perfect_toml')
+require('toml-rb')
 
 path_prefix = "#{__dir__}/../test/many_data/fixtures/dumped_fixed_examples_"
 timestamp = '2024-01-10_07-59'
@@ -13,8 +13,9 @@ without_ext = "#{path_prefix}#{timestamp}"
 dump_data = File.binread("#{without_ext}.bin")
 examples = Marshal.load(dump_data)
 toml_prepared = examples.sort_by(&:integer).to_h { |example| [example.string, example.to_h.except(:string, :period)] }
-PerfectTOML.save_file("#{without_ext}.toml", toml_prepared)
 
-if toml_prepared == PerfectTOML.load_file("#{without_ext}.toml")
+File.write("#{without_ext}.toml", TomlRB.dump(toml_prepared))
+
+if toml_prepared == TomlRB.load_file("#{without_ext}.toml")
   raise 'there is a problem for serialization or deserialization in snapshots. Guessing Bug or https://github.com/toml-lang/toml/issues/538#issuecomment-1266209110'
 end
